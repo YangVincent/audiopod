06402d12b07e5982d3ac5fb40269ac1bb57aa146
Vincent Yang <vincenty.376@gmail.com>
2018-08-29 17:09:02 -0700

    update shell script

--- generate_audiopod.sh
+++ generate_audiopod.sh
@@ -5,3 +5,6 @@
 # 2. Pass in public ip for ngrok tunnel to python script to generate xml
 # 3. Upload in Overcast
 echo "Hello from shell script"
+env/bin/python audiopod.py http://3895cb99.ngrok.io
+export FLASK_APP=audiopod.py
+flask run
